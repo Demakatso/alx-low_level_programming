faaac1897e04755c066fccb12ad14ba0251500d7
demakatso <demakatsomay@gmail.com>
2023-02-16 19:36:49 -0800

    betty 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,17 +3,15 @@
 /**
  * main - Entry point
  *
- * Description: Using sizeoff to print size of various types.
- *
- * Return: Return always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-return (0);
-}
+	printf("Size of a char: %1d byte(s)\n", sizeof(char));
+	printf("Size of an int: %1d byte(s)\n", sizeof(int));
+	printf("Size of an a long int: %1d byte(s)\n", sizeof(long int));
+	printf("Size of an a long long int: %1d byte(s)\n", sizeof(long long int));
+	printf("Size of an a float: %1d byte(s)\n", sizeof(float));
+	return (0);
+}				
