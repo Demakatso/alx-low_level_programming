7ec399a383dec705cc69d380408a771f8bb1f172
demakatso <demakatsomay@gmail.com>
2023-02-16 20:37:32 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 
 /**
 *main -Entry point
-*
+
 *Return: Always 0 (Success)
 */
 
@@ -14,4 +14,4 @@ int main(void)
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
-}			
+}
