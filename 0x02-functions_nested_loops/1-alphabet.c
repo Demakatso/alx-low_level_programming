367bea820ca79285ada79be69206d2c726f08f73
demakatso <demakatsomay@gmail.com>
2023-02-21 20:13:54 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,17 +2,16 @@
 
 /**
  * main - entry stream
- *
  * Return: Always 0
- *
  */
 int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++);
 		putchar(letter);
 
-	_putchar('\n');
-}
+	putchar('\n');
+	return (0);
+} 
 
