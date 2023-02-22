1b24e2d0d3aa731cd43a25338d8881d6a3963b05
demakatso <demakatsomay@gmail.com>
2023-02-21 19:22:50 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
 #include "main.h"
+
 /**
- *  * main - print _putchar
- * Return: Always 0
+ * main - main entry stream
+ * Return: 0 always success
  */
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
-}
-
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
