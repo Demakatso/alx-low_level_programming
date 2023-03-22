284fbee1a8e4f35f1d3ee62c8954cc5f2247b1e7
demakatso <demakatsomay@gmail.com>
2023-03-22 14:01:00 -0700

    3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,12 +1,9 @@
 #ifndef CALC_H
 #define CALC_H
 
-int (*get_op_func(char *s))(int, int);
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct op - Struct op
@@ -20,4 +17,11 @@ typedef struct op
 	int (*f)(int a, int b);
 } op_t;
 
-#endif /* CALC_H */
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
+
+#endif
