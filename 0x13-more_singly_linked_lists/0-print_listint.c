711efdbdb0c3c8eb1c22449893ec7b240af796ea
demakatso <demakatsomay@gmail.com>
2023-04-04 13:07:09 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,15 +9,16 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    const listint_t *current = h;
-    size_t count = 0;
+const listint_t *current = h;
+size_t count = 0;
 
-    while (current != NULL)
-    {
-        printf("%d\n", current->n);
-        count++;
-        current = current->next;
-    }
-
-    return (count);
+while (current != NULL)
+{
+	    printf("%d\n", current->n);
+	    count++;
+	    current = current->next;
 }
+
+return (count);
+}
+
