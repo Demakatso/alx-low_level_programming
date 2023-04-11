7d34d92fa6a2f1697a24a5622e6a4898f817aba0
demakatso <demakatsomay@gmail.com>
2023-04-11 12:04:21 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,45 +1,30 @@
 #include "main.h"
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdlib.h>
-
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: name of the file to read
- * @letters: number of letters it should read and print
- *
- * Return: 0 if failure, Else actual number of letters it could read and print
+ * append_text_to_file - This function adds text at EOF
+ * @filename: name of the file to append data to.
+ * @text_content: data to append to file
+ * Return: 1 if succesfull else -1 on err
  */
+int append_text_to_file(const char *filename, char *text_content)
+{
+int fild = 0, chk = 0, i = 0;
 
-ssize_t read_textfile(const char *filename, size_t letters)
-{
-int fd;
-ssize_t r, w;
-char *buffer;
+if (!filename)
+return (-1);
 
-if (filename == NULL)
-return (0);
-fd = open(filename, O_RDONLY);
-if (fd == -1)
-return (0);
-buffer = malloc(sizeof(char) * letters);
-if (buffer == NULL)
+fild = open(filename, O_WRONLY | O_APPEND);
+if (fild < 0)
+return (-1);
+
+if (text_content)
 {
-close(fd);
-return (0);
+while (text_content[i])
+i++;
+
+chk = write(fild, text_content, i);
+if (chk < 0)
+return (-1);
 }
-r = read(fd, buffer, letters);
-close(fd);
-if (r == -1)
-{
-free(buffer);
-return (0);
-}
-w = write(STDOUT_FILENO, buffer, r);
-free(buffer);
-if (r != w)
-return (0);
-return (w);
+close(fild);
+return (1);
 }
